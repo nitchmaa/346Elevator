734520842dfb9b813081c7a1debb508b3e74d3bc
MaddyMeng <burrelma@mail.gvsu.edu>
2023-11-30 19:50:24 -0500

    update

--- ElevatorCode_V3.1/ElevatorCode_V3.1.ino
+++ ElevatorCode_V3.1/ElevatorCode_V3.1.ino
@@ -21,11 +21,10 @@
 
 //FUNCTION PROTOTYPES
 void setUp(void); //configures pins; setup function
-int constantSpeed(); //adjust voltage for constant speed; return new voltage
-int proportionSpeed(int deltaX); //find volt to reduce speed proportional to distance from floor; receive dx, return new voltage
+void speedControl(double desiredSpeed); //PID control for speed
+void positionControl(double desiredPos); //PID control for close distances
 void moveDown (void); //feedback loop down
 void moveUp (void); //feedback loop up
-void idle (void);
 void readEncoder(void);
 
 //DEFINE GENERAL USE VARIABLES
@@ -33,148 +32,68 @@ int state = 1;
 char substate = 'A';
 
 int objectPlat = 0; //stores response from function for whether object has been detected
-int topX = 70; //x of top floor
-int botX = 0; //x of bottom floor
-float errorMargin = 0.25; //margin of error
-int xClose = 8; //to determine whether wanted - actual difference is small or large
-//volatile long voltage; //store current voltage being sent to motor
-
-
-
 
 //POSITION VALUES FOR CONTROL
- float theta =0;
- float thetaDot = 0;
- float x = 0;
- double xDot = 0;
-  long previousMillis = 0;
+double theta =0;
+double thetaDot = 0;
+double x = 0;
+double xDot = 0;
+long previousMillis = 0;
+
+int maxVolt = 190;
+int minVolt = 50;
 
 
 //PID
-double kp = 2.5, ki = 0.25, kd = 2;
-double desiredSpeed;
-long voltage = 100;
+double kp, ki, kd;
+volatile long voltage = 100;
 double totalError = 0, error = 0, eSignal = 0;
-//setpoint = desired speed (50)
-//error = actual speed - desired speed (50)
-//output = voltage
 
-//PID constPID(&xDot, &voltage, &desiredSpeed, kp, ki, kd, DIRECT);
 Encoder myEnc(2,3);
-long pos = -999;
+long pos = -999; //stores encoder position value
+
 long currTime = millis();
 long prevTime = currTime;
-float deltaTime = 0;
-float deltaTimeSec = 0;
-float deltaRad = 0;
-float prevRad = 0;
+double deltaTimeSec = 0;
+
+double deltaRad = 0;
+double prevRad = 0;
 
 double deltaError = 0;
 double prevError = 0;
 double errorDot = 0;
 
 
- //NEW FUNCTION CALL:
- //readThetaAndX(&theta, &thetaDot, &x, &xDot, &previousMillis);
- 
-
-
 
 void setup() {
   Serial.begin(9600); // Opens serial port and sets the data rate to 9600 bits-per-second
   
-  pinMode(LED_BUILTIN,OUTPUT);
-  digitalWrite(LED_BUILTIN, LOW);
-  
   setUp(); //function to set pins
   
-  myEnc.write(0);
-    currTime = millis();
-    prevTime = currTime;
-    deltaTime = currTime - prevTime;
+  myEnc.write(0); //reset encoder position
+  currTime = millis();
+  prevTime = currTime;
+  deltaTime = currTime - prevTime;
   
   //topActLoad(0.1); //pre-loop run of actuators
   //botActLoad(0.1);
   
-  //moveUp();
-  //constPID.SetMode(AUTOMATIC);
+  //moveUp(); //move car to top for start of loop
 }
 
 
 void loop() {
-   
-    int dir = 1;
-    double temp;
-    
-    motorUp();
-    voltage = 250;
-    motorSpeed(voltage);
-    delay(1000);
-    
-    while(1){
-      if(dir == 1){
-        desiredSpeed = 50;
-        
-        readEncoder();
-        
-                
-        error = desiredSpeed - xDot;
-        Serial.print("Error: ");
-        Serial.print(error);
-        Serial.println(" cm/s");
-
-        totalError = totalError + error;
-        Serial.print("Total Error: ");
-        Serial.println(totalError);
-
-        deltaError = error - prevError;
-        prevError = error;
-        errorDot = deltaError / deltaTime;
-        Serial.print("ErrorDot: ");
-        Serial.println(errorDot);
-        
-        eSignal = kp*error + ki*totalError + kd*errorDot;
-        Serial.print("eSignal: ");
-        Serial.println(eSignal);
-
-        voltage = voltage + eSignal;
-        if(voltage > 250) voltage = 250;
-        else if (voltage < 50) voltage = 50;
-        //Serial.print("Voltage: ");
-        //Serial.print(voltage);
-        //Serial.println(" V");
-        
-        //constPID.Compute();
-        motorSpeed(voltage);
-        Serial.println("------------------------------------");
-        
-        //if(x >= 50) dir = 2;
-      }
-      else if(dir == 2){
-        desiredSpeed = -5;
-        motorDown();
-        readEncoder();
-        //readThetaAndX(&theta, &thetaDot, &x, &xDot, &previousMillis);
-        //constPID.Compute();
-        motorSpeed(voltage);
-
-        if(x <= 10) dir = 1;
-      }
-    }
-
-
-    
     switch (state) {
       case 1:
         if(substate == 'A'){
-          idle();
           Serial.println("State 1A");
+
+          //positionControl(70); //hold at top
+          
           objectPlat = objectDetect(); //is an object detected? if no (return of 0), do nothing
-          idle();
           if(objectPlat == 1){ //if on top, load top actuator, set to state 3-B
             topActLoad(0.3125);
-            Serial.println("actuating");
-            delay(5000);
+            Serial.println("Top actuating");
             state = 3;
             substate = 'B';
           }
@@ -182,14 +101,15 @@ void loop() {
             state = 3;
           }
         }
-      
         else if(substate == 'B'){
           Serial.println("State 1B");
-          stopMotor(); //stop motor. Path B has finished.        
+          //positionControl(70); //hold at top. Path B has finished.        
         }
-
         else if(substate == 'C'){
           Serial.println("State 1C");
+
+          //positionControl(70); //hold at top
+          
           delay(1000); //pause 1 sec, then set to state 3
           state = 3;
         }
@@ -198,40 +118,43 @@ void loop() {
       case 2:
         if(substate == 'A'){
           Serial.println("State 2A");
+
+          //positionControl(0); //hold at bottom
+          
           objectPlat = objectDetect(); //is an object detected? if no, do nothing
-          if(objectPlat == 2){ //if on bottom, load bottom actuator, set to state 4-C (should never be on top floor here)
+          if(objectPlat == 2){ //if on bottom, load bottom actuator, set to state 4-C (object should never be on top floor here)
             botActLoad(0.3125);
             state = 4;
             substate = 'C';
           }
         }
-      
         else if(substate == 'B'){
-          Serial.println("State 2B");   
+          Serial.println("State 2B");
+
+          //positionControl(0); //hold at bottom
+          
           delay(1000); //pause 1 sec, then set to state 4
           state = 4;
         }
-
         else if(substate == 'C'){
-          stopMotor(); //stop motor. Path C has finished. 
+          //positionControl(0); //hold at bottom. Path C has finished. 
         }
         break;
       
       case 3:
         moveDown(); //enter feedback loop, moving down, then set to state 2
         state = 2;
-         substate = 'A';
+        //substate = 'A';
         break;
       
       case 4:
         moveUp(); //enter feedback loop, moving up, then set to state 1
         state = 1;
-        substate = 'B';
+        //substate = 'B';
         break;
       
       default:
         Serial.println("Error in switch case. Stopping motor.");
-        digitalWrite(LED_BUILTIN, HIGH);
         stopMotor();
         break;
     }
@@ -275,218 +198,163 @@ void setUp(void){ //setup function
   previousMillis = millis();
 }
 
-int constantSpeed(void){ //adjust voltage for constant speed; return new voltage
-  /*
-  int voltAdjust; //value to be returned
-  double Kc = 0.1;
-  int speedSignal;
-  int speedDiff;
+void speedControl(double desiredSpeed){ //PID control for speed
+  kp = 2.5;
+  ki = 0.25;
+  kd = 2;
+  
+  readEncoder();
 
-  speedDiff = xDot - 50; //find speed difference
-  speedSignal = Kc*speedDiff; //find proportion for speed change
-  voltAdjust = voltage + speedSignal; //calculate new voltage
+  error = desiredSpeed - xDot; //find Kp error
+        //Serial.print("Error: ");
+        //Serial.print(error);
+        //Serial.println(" cm/s");
 
-  voltage = voltAdjust; //update global voltage
+  deltaError = error - prevError;
+  prevError = error;
+  totalError = totalError + error*deltaTime; //find Ki error
+        //Serial.print("Total Error: ");
+        //Serial.println(totalError);
 
-  return voltAdjust;
+  errorDot = deltaError / deltaTime;
+        //Serial.print("ErrorDot: ");
+        //Serial.println(errorDot);
+        
+  eSignal = kp*error + ki*totalError + kd*errorDot;
+  
+        Serial.print("eSignal: ");
+        Serial.println(eSignal);
+
+  voltage = voltage + eSignal;
+  if(voltage > maxVolt) voltage = maxVolt;
+  else if (voltage < minVolt) voltage = minVolt;
+        //Serial.print("Voltage: ");
+        //Serial.print(voltage);
+        //Serial.println(" V");
+        
+  //Serial.println("------------------------------------");
 }
 
-int proportionSpeed(int deltaX){ //find volt to reduce speed proportional to distance from floor; receive dx, return new voltage
-  int voltAdjust; //value to be returned
-  int minUnload = 50; //minimum voltage to move motor when unloaded
-  int minLoad = 75; //minimum voltage (approx) to move motor when loaded
-  
-  double kp = 0.1;
-  int speedSig = kp*deltaX;
-  voltAdjust = voltage - speedSig; //calculate new voltage
 
-//  voltAdjust = kp*voltAdjust; //adjust voltage to percrentage
-  if(substate == 'A' && voltAdjust < minUnload){ //unloaded, minimum voltage needed to move
-    voltAdjust = minUnload;
-  }
-  if(voltAdjust < minLoad){ //loaded, minimum voltage needed to move
-    voltAdjust = minLoad;
-  }
+void positionControl(double desiredPos){ //PID control for close distances
+  kp = 2.5;
+  ki = 0.25;
+  kd = 2;
   
-  voltage = voltAdjust; //update global voltage
+  readEncoder();
+
+  error = desiredPos - x;
+        //Serial.print("Error: ");
+        //Serial.print(error);
+        //Serial.println(" cm/s");
+
+  deltaError = error - prevError;
+  prevError = error;
+  totalError = totalError + error*deltaError;
+        //Serial.print("Total Error: ");
+        //Serial.println(totalError);
+
+  errorDot = deltaError / deltaTime;
+        //Serial.print("ErrorDot: ");
+        //Serial.println(errorDot);
+        
+  eSignal = kp*error + ki*totalError + kd*errorDot;
   
-  return voltAdjust;
-  */
+        Serial.print("eSignal: ");
+        Serial.println(eSignal);
+
+  voltage = voltage + eSignal;
+  if(voltage > maxVolt) voltage = maxVolt;
+  else if (voltage < minVolt) voltage = minVolt;
+        //Serial.print("Voltage: ");
+        //Serial.print(voltage);
+        //Serial.println(" V");
+        
+  //Serial.println("------------------------------------");
 }
-
 
 
 void moveDown (void){ //feedback loop down
-/*
- int initialVolt = 10;
-  int counter = 0;
-  int finished = 0;
-  int volt;
-  float dx;
-  float kp = 3;
-
-  Serial.println("Feedback loop, up.");
-
-  voltage = initialVolt; //update global voltage
-
-  while(finished != 1){ //loop until elevator stablized at destination
-    readThetaAndX(&theta, &thetaDot, &x, &xDot, &previousMillis); //get current position and speed
-
-    if(xDot > -1){
-      voltage = voltage - 1;
-       Serial.println("running the rundown 1");
-    }
-    if(x > 1){
-      voltage = voltage-1;
-       Serial.println("running the rundown2");
-    }
-    if(voltage <1){
-      voltage = 1;
-       Serial.println("running the rundown3");
-    }
-    if(x < 1){
-     finished = 1;
-     Serial.println("running the rundown 4 done");
-     delay(5000);
-    }
-  
-      //motorUp(); //set direction up
-      motorSpeed(voltage);
-    }
-    */
+//copy moveUp here when finished
 }
 
 
 
 void moveUp (void){ //feedback loop up
-/*
-  int initialVolt = 80;
-  int counter = 0;
   int finished = 0;
-  int volt;
-  float dx;
-  float kp = 3;
 
   Serial.println("Feedback loop, up.");
 
-  voltage = initialVolt; //update global voltage
-
-  while(finished != 1){ //loop until elevator stablized at destination
-    readThetaAndX(&theta, &thetaDot, &x, &xDot, &previousMillis); //get current position and speed
-
-    if(x > (69)){ //x at top floor within margin of error
-        voltage = voltage/3.0;
-        finished = 1;
-        Serial.print("moveup function");
+  motorUp();
+  
+  while(finished != 1){ //loop in function until elevator stablized at destination
+    if(x > 69.5){ //x at top floor within margin of error, exit moveUp function
+      finished = 1;
     }
-    else if(x < 69){ //x below floor
-      if(xDot < 1){
-        voltage = voltage + 1;
-        if(voltage > 255){
-          voltage = 255;
-        }
-      }
-      
-      motorUp(); //set direction up
+    else if(x > 63){ //position control above this point
+      positionControl(70); //position control, want 70 cm
+      motorSpeed(voltage);
+    }
+    else{
+      speedControl(50); //constant speed control, want 50 cm/s
       motorSpeed(voltage);
     }
   }
-  */
 }
 
-
-void idle (void){ //idle
-/*
-  int initialVolt = 30;
-  int counter = 0;
-  int finished = 0;
-  int volt;
-  float dx;
-  float kp = 10;
-
-  Serial.println("Idling.");
-
-  voltage = initialVolt; //update global voltage
-
-  readThetaAndX(&theta, &thetaDot, &x, &xDot, &previousMillis); //get current position and speed
-
-    if(x > (69)){ //x at top floor within margin of error
-        volt = voltage/3;
-        motorSpeed(volt);
-        Serial.println("1");
-    }
-    else if(x <= 69){ //x below floor
-      Serial.println("2");
-      motorUp(); //set direction up
-      //if(xDot < 70){
-        voltage = initialVolt + kp*(topX - x);
-        if(xDot < 1){
-          initialVolt++;
-          Serial.println("3");
-        }
-        if(voltage > 255){
-          voltage = 255;
-          Serial.println("4");
-        }
-        motorSpeed(voltage);
-  }
-  */
-}
 
 void readEncoder(void){
   long newPos;
-  float rad, radDot;
-  float degree, degreeDot;
+  double rad, radDot;
+  double degree, degreeDot;
 
   currTime = millis();
   deltaTime = (currTime - prevTime);
+  deltaTimeSec = deltaTime/1000;
 
   if(deltaTime > 10){
     prevTime = currTime;
     
     newPos=myEnc.read();
     
-      //Serial.print("DeltaTime: ");
-      //Serial.println(deltaTime);
-      deltaTimeSec = deltaTime/1000;
-      pos=newPos;
+    pos=newPos;
 
-      //-440 encoder count for one rev
-      rad = (float)pos/-440.0*(2.0 * 3.14159); //radians
+    //-440 encoder count for one rev
+    rad = (double)pos/-440.0*(2.0 * 3.14159); //radians
       
-      deltaRad = rad - prevRad;
-      prevRad = rad;
+    deltaRad = rad - prevRad;
+    prevRad = rad;
       
-      radDot = deltaRad/deltaTimeSec; //rad/s
-      degree = rad*180/3.14159;
-      degreeDot = radDot*180/3.14159;
-      x=((float)(rad * 1.358)); //cm
-      xDot=((float)(radDot * 1.358)); //cm
-      /*
-      Serial.print('\t');
-      Serial.print("Theta, R: ");
-      Serial.print(rad);
-      Serial.println(" Rad");
+    radDot = deltaRad/deltaTimeSec; //rad/s
+    degree = rad*180/3.14159;
+    degreeDot = radDot*180/3.14159;
+    x=((double)(rad * 1.358)); //cm
+    xDot=((double)(radDot * 1.358)); //cm
+    /*
+    Serial.print('\t');
+    Serial.print("Theta, R: ");
+    Serial.print(rad);
+    Serial.println(" Rad");
       
-      Serial.print('\t');
-      Serial.print("Theta, D: ");
-      Serial.print(degree);
-      Serial.println(" degrees");
+    Serial.print('\t');
+    Serial.print("Theta, D: ");
+    Serial.print(degree);
+    Serial.println(" degrees");
       
-      Serial.print('\t');
-      Serial.print("Theta Dot: ");
-      Serial.print(degreeDot);
-      Serial.println(" Deg/sec");
+    Serial.print('\t');
+    Serial.print("Theta Dot: ");
+    Serial.print(degreeDot);
+    Serial.println(" Deg/sec");
       
-      Serial.print('\t');
-      Serial.print("X ");
-      Serial.print(x);
-      Serial.println(" cm");
-      */
-      Serial.print('\t');
-      Serial.print("XDot ");
-      Serial.print(xDot);
-      Serial.println(" cm/s");
+    Serial.print('\t');
+    Serial.print("X ");
+    Serial.print(x);
+    Serial.println(" cm");
       
+    Serial.print('\t');
+    Serial.print("XDot ");
+    Serial.print(xDot);
+    Serial.println(" cm/s");
+    */
   }
 }
